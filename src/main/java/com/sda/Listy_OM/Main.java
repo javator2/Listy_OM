7cafda945c27aa7a5bdb8deec2555a9a53930a53
Olek <oleksiy.mysko@gmail.com>
2018-07-28 15:54:51 +0200

    koniec zajec

--- src/main/java/com/sda/Listy_OM/Main.java
+++ src/main/java/com/sda/Listy_OM/Main.java
@@ -1,5 +1,26 @@
 package com.sda.Listy_OM;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) {
+     CustomList<Integer> list = new CustomList<Integer>();
+     list.add(2);
+     list.add(2);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+     list.add(12);
+
+        for (CustomList list1: list){
+            System.out.println(list1);
+        }
+    }
 }
